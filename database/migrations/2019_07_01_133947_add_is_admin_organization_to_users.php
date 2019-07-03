aa667abb74b81cfb519e6f7265e6eb5559b8ae69
Galih Akbar Moerbayaksa <moer@student.uns.ac.id>
2019-07-03 07:02:11 +0700

    migrations: fix error migration default value

--- database/migrations/2019_07_01_133947_add_is_admin_organization_to_users.php
+++ database/migrations/2019_07_01_133947_add_is_admin_organization_to_users.php
@@ -14,7 +14,7 @@ class AddIsAdminOrganizationToUsers extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->boolean('is_admin_organization');
+            $table->boolean('is_admin_organization')->default(false);
         });
     }
 
